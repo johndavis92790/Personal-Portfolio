e85eb87b54e4d3999c7f69cedae732948d5b5512
johndavis <johndavis92790@gmail.com>
2022-03-26 21:27:06 -0600

    fixed resume lists

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -12,18 +12,22 @@ function Resume() {
             <p className="mt-3">View my resume</p>
           </a>
           <div>
-            <p className="mt-3">
-              Top Languages/Technologies: Javascript, React, Express.js,
-              MongoDB, Node.js, HTML, CSS.
-            </p>
+            <p className="mt-3">Front-End Proficiencies:</p>
             <ul>
               <li>Javascript</li>
               <li>React</li>
-              <li>Express.js</li>
-              <li>MongoDB</li>
-              <li>Node.js</li>
               <li>HTML</li>
               <li>CSS</li>
+              <li>Bootstrap</li>
+              <li>Bulma</li>
+            </ul>
+            <p className="mt-3">Back-End Proficiencies:</p>
+            <ul>
+              <li>Express.js</li>
+              <li>Node.js</li>
+              <li>REST API's</li>
+              <li>MongoDB, Mongoose</li>
+              <li>MySQL, Sequelize</li>
             </ul>
           </div>
         </Col>
